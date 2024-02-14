f6682f2e712118c1eeae8e87a8b2d5896ce47b5c
Daniel Vidal <danielsv465@hotmail.com>
2024-02-13 22:52:08 -0300

    Script improvements

--- code-commit-clone-repositories-automation.sh
+++ code-commit-clone-repositories-automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "Start repository clones... \n"
+echo "Start repository clones..."
 
 # Configurações AWS CLI
 AWS_PROFILE="" # Insira o nome do perfil de usuário do AWS CLI que será usado para acessar o CodeCommit. Ex: "default"
@@ -21,27 +21,32 @@ create_directory_if_not_exists "$BACKEND_DIR"
 create_directory_if_not_exists "$FRONTEND_DIR"
 
 # Prefixos dos repositórios a serem clonados
-BACKEND_REPO_START_WITH=() # Coloque os prefixos dos repositórios backend aqui. Ex: BACKEND_REPO_START_WITH=("backend-" "data-")
-FRONTEND_REPO_START_WITH=() # Coloque os prefixos dos repositórios frontend aqui. Ex: FRONTEND_REPO_START_WITH=("frontend-" "components-")
+BACKEND_REPOS_START_WITH=() # Coloque os prefixos dos repositórios backend aqui. Ex: BACKEND_REPOS_START_WITH=("backend-" "data-")
+FRONTEND_REPOS_START_WITH=() # Coloque os prefixos dos repositórios frontend aqui. Ex: FRONTEND_REPOS_START_WITH=("frontend-" "components-")
 
 # Obtém a lista de repositórios
 repositories=$(AWS_PROFILE="$AWS_PROFILE" aws codecommit list-repositories --query 'repositories[].repositoryName' --output text)
 
-# Itera pelos repositórios
-for repository in $repositories; do
-  # Clona os repositórios backend que começam com os prefixos especificados
-  for prefix in ${BACKEND_REPO_START_WITH[@]}; do
-    if [[ $repository == $prefix* ]]; then
-      git clone ssh://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$repository "$BACKEND_DIR/$repository"
-    fi
-  done
+clone_repositories() {
+  local prefix=$1
+  local dir=$2
 
-  # Clona os repositórios frontend que começam com os prefixos especificados
-  for prefix in ${FRONTEND_REPO_START_WITH[@]}; do
-    if [[ $repository == $prefix* ]]; then
-      git clone ssh://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$repository "$FRONTEND_DIR/$repository"
+  for repository in $repositories; do
+    # Verifica se o repositório começa com o prefixo especificado e se o diretório não existe, então clona o repositório.
+    if [[ $repository == $prefix* && ! -d "$dir/$repository" ]]; then
+      git clone "ssh://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$repository" "$dir/$repository"
     fi
   done
+}
+
+# Clona os repositórios backend que começam com os prefixos especificados
+for prefix in "${BACKEND_REPOS_START_WITH[@]}"; do
+  clone_repositories "$prefix" "$BACKEND_DIR"
 done
 
-printf "End repository clones... \n"
+# Clona os repositórios frontend que começam com os prefixos especificados
+for prefix in "${FRONTEND_REPOS_START_WITH[@]}"; do
+  clone_repositories "$prefix" "$FRONTEND_DIR"
+done
+
+echo "End repository clones..."
